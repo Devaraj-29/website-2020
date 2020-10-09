dc535e64093cc2231325cd5bc88b5c819f05dee6
Devaraj-29 <69611795+Devaraj-29@users.noreply.github.com>
2020-10-09 14:29:59 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+
+    //owl carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true
+    });
+
+})
